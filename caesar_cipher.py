423a7a5949e1fa9f57b30c46d35f674ff6999531
Bennibraun <benjaminbraun@live.com>
2018-02-14 13:31:51 -0500

    Add files via upload

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,16 +1,26 @@
 def shift(letter, n):
-    pass
+    return chr(ord(letter)+n)
 
 def encrypt(message, shift_amount):
-    pass
+    result = ''
 
+    for letter in message:
+        result += shift(letter,shift_amount)
+
+    return result
 
 def decrypt(message, shift_amount):
-    pass
+    result = ''
+
+    for letter in message:
+        result += shift(letter, -(shift_amount))
+
+    return result
 
 secret_message = "encryption is fun"
 encrypted_message = encrypt(secret_message, 3)
+decrypted_message = decrypt(encrypted_message, 3)
 
+print(secret_message)
 print(encrypted_message)
-
-
+print(decrypted_message)
--- substitution_cipher.py
+++ substitution_cipher.py
@@ -1,15 +1,38 @@
-alphabet = "abcdefghijklmnopqrstuvwxyz "
-key = "qwertyuioplkjhgfdsazxcvbnm "
+import random
+
+def random_chars():
+    result = ''
+    for i in range(32,127):
+        result += chr(i)
+    return ''.join(random.sample(result,len(result)))
+
+alphabet = random_chars()
+key = random_chars()
 
 def encrypt(message):
-    pass
+    loc = 0
+    result = ''
+
+    for letter in message:
+        loc = alphabet.find(letter)
+        result += key[loc]
+
+    return result
 
 def decrypt(message):
-    pass
+    loc = 0
+    result = ''
 
+    for letter in message:
+        loc = key.find(letter)
+        result += alphabet[loc]
+
+    return result
 
 unencrypted_message = "encryption is fun"
 encrypted_message = encrypt(unencrypted_message)
+decrypted_message = decrypt(encrypted_message)
 
 print(unencrypted_message)
 print(encrypted_message)
+print(decrypted_message)
